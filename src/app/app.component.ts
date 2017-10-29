85f4084aa3de07a1bab13f454bb9caea3fabde2f
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-10-29 11:29:18 +0100

    reading list  + reading object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 import {AngularFireDatabase} from "angularfire2/database";
 import {Observable} from "rxjs";
 
@@ -9,8 +9,12 @@ import {Observable} from "rxjs";
 })
 export class AppComponent {
   courses: Observable<any[]>;
+  authors: Observable<any[]>;
+  firstCourse: Observable<any>;
 
   constructor(db: AngularFireDatabase) {
     this.courses = db.list('courses').valueChanges();
+    this.firstCourse = db.object('/courses/1').valueChanges();
+    this.authors = db.list('/authors').valueChanges();
   }
 }
