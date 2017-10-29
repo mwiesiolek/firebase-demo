959420ded21282accb4094310639d4d0fe92dcc3
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-10-29 11:02:14 +0100

    list data from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AngularFireDatabase} from "angularfire2/database";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  courses: Observable<any[]>;
+
+  constructor(db: AngularFireDatabase) {
+    this.courses = db.list('courses').valueChanges();
+  }
 }
