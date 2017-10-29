6208a839ac8fe4063ff3aa56aa87e1715b37dc0e
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-10-29 11:37:43 +0100

    showing object if available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,15 @@ import {Observable} from "rxjs";
 })
 export class AppComponent {
   courses: Observable<any[]>;
-  authors: Observable<any[]>;
   firstCourse: Observable<any>;
+
+  authors: Observable<any[]>;
+  firstAuthor: Observable<any>;
 
   constructor(db: AngularFireDatabase) {
     this.courses = db.list('courses').valueChanges();
     this.firstCourse = db.object('/courses/1').valueChanges();
     this.authors = db.list('/authors').valueChanges();
+    this.firstAuthor = db.object('/authors/1').valueChanges();
   }
 }
