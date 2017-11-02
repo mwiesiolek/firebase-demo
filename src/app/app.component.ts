ed58a8113d728dcadfd9bb76ee4b18c8b5594d3e
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-11-02 18:36:46 +0100

    adding new object to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-import {AngularFireDatabase} from "angularfire2/database";
+import {AngularFireDatabase, AngularFireList} from "angularfire2/database";
 import {Observable} from "rxjs";
 
 @Component({
@@ -8,6 +8,7 @@ import {Observable} from "rxjs";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  courses$: AngularFireList<any>;
   courses: Observable<any[]>;
   firstCourse: Observable<any>;
 
@@ -15,9 +16,25 @@ export class AppComponent {
   firstAuthor: Observable<any>;
 
   constructor(db: AngularFireDatabase) {
-    this.courses = db.list('courses').valueChanges();
+    this.courses$ = db.list('courses');
+    this.courses = this.courses$.valueChanges();
     this.firstCourse = db.object('/courses/1').valueChanges();
+
     this.authors = db.list('/authors').valueChanges();
     this.firstAuthor = db.object('/authors/1').valueChanges();
+  }
+
+  add(course: HTMLInputElement) {
+    this.courses$.push({
+      name: course.value,
+      price: 150,
+      isLive: true,
+      sections: [
+        { title: 'Components' },
+        { title: 'Directives' },
+        { title: 'Templates' }
+      ]
+    });
+    course.value = '';
   }
 }
